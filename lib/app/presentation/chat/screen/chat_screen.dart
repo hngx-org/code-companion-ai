5f43425b5aeacacc473a528db44da27957549b0a
Daveadj <ovoadjerese@gmail.com>
2023-10-02 14:36:18 +0100

    add: screens

---
+++ lib/app/presentation/chat/screen/chat_screen.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final List<MessageModel> messages = [];
+  TextEditingController messageController = TextEditingController();
+
+  ScrollController scrollController = ScrollController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    messageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Chat Buddy'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              controller: scrollController,
+              itemCount: messages.length,
+              itemBuilder: (context, index) {
+                final message = messages[index];
+                return MessageBubble(
+                    message: message.text, isUser: message.isUser);
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    controller: messageController,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a message';
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration.collapsed(
+                      hintText: 'Type your message...',
+                    ),
+                  ),
+                ),
+                IconButton(onPressed: () {}, icon: const Icon(Icons.send))
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class MessageModel {
+  final int id;
+  final String text;
+  final bool isUser;
+
+  MessageModel({required this.id, required this.text, required this.isUser});
+}
+
+class MessageBubble extends StatelessWidget {
+  final String message;
+  final bool isUser;
+
+  const MessageBubble({super.key, required this.message, required this.isUser});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: isUser ? Alignment.centerRight : Alignment.centerLeft,
+      margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+      child: Container(
+        padding: const EdgeInsets.all(12.0),
+        decoration: BoxDecoration(
+          color: isUser ? Colors.blue : Colors.grey,
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        child: Text(
+          message,
+          style: const TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
--- lib/app/presentation/onboarding/screens/onboarding.dart
+++ lib/app/presentation/onboarding/screens/onboarding.dart
@@ -1,3 +1,4 @@
+import 'package:code_companion_ai/app/presentation/auth/login/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 
 class OnboardingScreen extends StatelessWidget {
@@ -5,6 +6,144 @@ class OnboardingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text(
+              'AI CodeCampers',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
+                  color: Colors.black),
+            ),
+            Image.asset(
+              'assets/images/pic.png',
+              height: 400,
+            ),
+            const Text(
+              ' your one-stop solution for seamless code debugging and expert coding assistance through chat! ',
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            CustomContainer(
+              fillColor: Colors.purpleAccent,
+              width: 280,
+              height: 50,
+              useShadow: false,
+              borderColor: Colors.white,
+              borderRadius: 40,
+              child: Center(
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Get Started',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.black),
+                  ),
+                ),
+              ),
+            ),
+            CustomContainer(
+              fillColor: Colors.purpleAccent,
+              width: 280,
+              height: 50,
+              useShadow: false,
+              borderColor: Colors.white,
+              borderRadius: 40,
+              child: Center(
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (ctx) => const LoginScreen()));
+                  },
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.black),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomContainer extends StatelessWidget {
+  // You can choose to set fill color
+  final Color fillColor;
+  // Set the width of the container
+  final double width;
+  // Set the height of the container
+  final double height;
+  // Set the borderRadius of the container
+  final double borderRadius;
+  // Color of the border of the container
+  final Color borderColor;
+  // Set how far the shadow extends
+  final double shadowOffset;
+  // Do I need shadow for the container of not
+  final bool useShadow;
+  // Content of the container
+  final Widget? child;
+  // Padding to apply to the children
+  final EdgeInsets? padding;
+  // Border Width
+  final double borderWidth;
+  // Alignment
+  final Alignment? alignment;
+
+  const CustomContainer({
+    super.key,
+    required this.fillColor,
+    required this.width,
+    required this.height,
+    this.borderColor = Colors.black,
+    this.borderRadius = 8.0,
+    this.shadowOffset = 4.0,
+    this.useShadow = true,
+    this.child,
+    this.padding,
+    this.borderWidth = 2,
+    this.alignment,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: alignment,
+      width: width,
+      height: height,
+      padding: padding ?? const EdgeInsets.all(0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(borderRadius),
+        border: Border.all(
+          color: borderColor,
+          width: borderWidth,
+        ),
+        color: fillColor,
+        boxShadow: useShadow
+            ? [
+                BoxShadow(
+                  color: Colors.black,
+                  blurRadius: 0,
+                  offset: Offset(shadowOffset, shadowOffset),
+                  spreadRadius: 0,
+                )
+              ]
+            : null,
+      ),
+      child: child ?? const SizedBox(),
+    );
   }
 }
